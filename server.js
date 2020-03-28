404964bd9fdaf1994b46e41f15672cbfb33f06f0
Sam Sutherland <samuelasutherland@gmail.com>
2020-03-28 03:14:40 +0000

    Openshift

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
 
 
