00496c86d6a985d1afb41d374090ec59fc625293
Sam Sutherland <samuelasutherland@gmail.com>
2020-06-17 21:30:57 +0100

    Fixing 0 cards bug

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
 const game_title = process.env.GAME_TITLE || 'Online Mao'
 const password = process.env.PASSWORD || ""
-const defaultNumStartingCards = parseInt(process.env.NUM_STARTING_CARDS) || 5
+const defaultNumStartingCards = (process.env.NUM_STARTING_CARDS === undefined) ? 5 : parseInt(process.env.NUM_STARTING_CARDS)
 console.log("Environment Variables")
 console.log("Num Starting Cards: "+defaultNumStartingCards)
 console.log("Title: "+game_title)
